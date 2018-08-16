4accaace4fcffa525802f62cabcf7d77090c3748
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-08-17 00:38:01 +0100

    Registering Auth and wrapping the App around the route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Auth from './Auth/Auth';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const auth = new Auth();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App auth={auth} />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
